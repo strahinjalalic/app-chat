c2fea927b59732702fb8e548dcf74e306101999f
someone <someone@something.com>
2019-03-24 14:02:35 +0100

    Added first socket action listeners and emit

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+var socket = io();//pravimo zahtev od klijenta ka serveru za otvaranje web socketa i odrzavanje te konekcije otvorenom
+  	socket.on("connect", function() {
+  		console.log("Connected to server");//metod se izvrsava na klijent strani
+
+  		socket.emit("createMessage", {
+  			from: "Mike",
+  			text: "Best regards from our team!"
+  		});
+  	});
+
+  	socket.on("newMessage", function(message) {//argument koji prosledjujemo je zapravo objekat kreiran na server strani
+  		console.log("newMessage", message);
+  	});
+
+  	socket.on("disconnect", function() {//arrow functions ce se renderovati samo na chrome-u, zato se koriste regular functions
+  		console.log("Disconnected from server");
+  	});
--- server/server.js
+++ server/server.js
@@ -1,15 +1,36 @@
 const path = require("path"); //built-in module, ne mora da se instalira u terminalu
 const publicPath = path.join(__dirname + "../public"); //izlazi se iz server foldera i ulazi u public => koristi se join() zato sto u apsolutnoj putanji ne belezi ulazak u server pa izlazak iz njega, vec samo ulazak u public 
+var http = require("http");
+var socketIO = require("socket.io");
 const express = require("express");
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app); // ovaj metod je inace deo listen() metoda, ali je pravilnije listen() pozivati sa server promenljivom u ovom slucaju
+var io = socketIO(server); // konfigursanje servera da takodje koristi socketIO
+
 app.use(express.static(publicPath));
 
 
+io.on("connection", (socket) => {//server izvrsava neku akciju kada se novi user konektuje
+	console.log("New user connected");
+
+	socket.emit("newMessage", {//server emituje akciju ka klijentu emit() metodom, ime metoda mora da se podudara sa imenom na klijent strani
+		from: "Andrew",
+		text: "Hey. What's going on?",
+		createdAt: 123
+	});
+
+	socket.on("createMessage", (message) => {
+		console.log("New message", message);
+	})
+
+	socket.on("disconnect", () => {
+		console.log("User disconnected");//printuje se u terminalu kad se iskljuci chrome(tj. tab u kome prikazujemo stranicu)
+	});
+}); 
 
 
-
-app.listen(3000, () => {
+server.listen(port, () => {
 	console.log(`Server je pokrenut na portu ${port}`);
 });
